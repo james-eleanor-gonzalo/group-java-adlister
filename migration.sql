18aa01073498cf8f6cbfaa2e14adf9f09f5c65df
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-02-03 10:04:11 -0600

    updated migration file, created seeder file

--- migration.sql
+++ migration.sql
@@ -2,6 +2,8 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ad_categories;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -16,7 +18,25 @@ CREATE TABLE ads (
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
+    category VARCHAR(240) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    categories VARCHAR(240) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ad_categories (
+    ad_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    ad_category VARCHAR(240) NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES ads(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+        ON DELETE CASCADE
+);
+
