6aa7330cd5b78d1ea59c3b970fc2a9a0fbf0143c
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-02-03 14:39:15 -0600

    users are now able to update profile info

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,8 +1,9 @@
 package com.codeup.adlister.dao;
 
-import com.codeup.adlister.dao.Config;
+import com.adlister.dao.Config;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
 
@@ -49,6 +50,38 @@ public class MySQLUsersDao implements Users {
             return rs.getLong(1);
         } catch (SQLException e) {
             throw new RuntimeException("Error creating new user", e);
+        }
+    }
+
+    @Override
+    public String hashPassword(String password) {
+        int numberOfRounds = 12;
+        return BCrypt.hashpw(password, BCrypt.gensalt(numberOfRounds));
+    }
+
+    @Override
+    public void changeEmail(String emailUpdate, String username) {
+        String query = "UPDATE users SET email =? WHERE username = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, emailUpdate);
+            stmt.setString(2, username);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error updating email.", e);
+        }
+    }
+
+    @Override
+    public void changePassword(String password, String username) {
+        String query = "UPDATE users SET password =? WHERE username = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, hashPassword(password));
+            stmt.setString(2, username);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error updating password.", e);
         }
     }
 
